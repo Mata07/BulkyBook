399c40f8937ee9509741e035e57e41c05d11a30c
Mata07 <mmatisic7@gmail.com>
2021-04-15 20:56:43 +0200

    Section 6 - Category Repository and Interface

---
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using BulkyBook.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        // get ApplicationDbContext and pass it to base ctor
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                // later implement Save in UnitOfWork - to _unitOfWork.Save();
+                _db.SaveChanges();
+            }            
+        }
+    }
+}
