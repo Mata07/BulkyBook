214705e152223562932115e67cb2237ca75c3842
Mata07 <mmatisic7@gmail.com>
2021-04-19 07:24:04 +0200

    Section 6 - Implement Stored Procedure Interface

---
+++ BulkyBook.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    // Repository for Stored procedures
+    public interface ISP_Call
+    {
+        // using Dapper to pass parameters
+        // Single<T> uses Execute Scalor that returns int or bool (expl - result first column in first row)
+        T Single<T>(string procedureName, DynamicParameters param = null);
+
+        void Execute(string procedureName, DynamicParameters param = null);
+
+        // Retrieve complete row
+        T OneRecord<T>(string procedureName, DynamicParameters param = null);
+
+        // Get All rows
+        IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
+
+        // Return two tables
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);
+
+    }
+}
