8c333c977d21c5f000cce3c4bb2425f3a52efe83
Mata07 <mmatisic7@gmail.com>
2021-04-21 10:08:13 +0200

    Section 9 - Product Controller

---
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,121 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        // for images in wwwroot folderu
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // One for Create and Edit - Upsert        
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                // get list of categories for dropdown
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+
+                // get list of covertypes for dropdown
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+
+            if (id == null)
+            {
+                // this is for Create new Product
+                return View(productVM);
+            }
+
+            // this is for Edit Product
+            productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            if (productVM == null)
+            {
+                return NotFound();
+            }
+
+            return View(productVM);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (product.Id == 0)
+        //        {
+        //            _unitOfWork.Product.Add(product);
+        //        }
+        //        else
+        //        {
+        //            _unitOfWork.Product.Update(product);
+        //        }
+        //        _unitOfWork.Save();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View(product);
+        //}
+
+
+        #region API CALLS
+
+        // using Api calls for Index because of using DataTables lib in View
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // pass includeProperties to include Category and CoverType for view
+            var allObj = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+
+        #endregion
+    }
+}
