b0d931ba751a42a141f7171ee834e658128b3c53
Mata07 <mmatisic7@gmail.com>
2021-04-21 09:23:46 +0200

    Section 9 - Add Product Model and push to Db

---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        // Prices by quantity        
+        [Required]
+        [Range(1, 10000)]
+        public double ListPrice { get; set; }
+
+        // q < 50       
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        // 50-99       
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+
+        // q > 100        
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [Required]
+        public int CoverTypeId { get; set; }
+        [ForeignKey("CoverTypeId")]
+        public CoverType CoverType { get; set; }
+
+    }
+}
