5baf58735cc38653e143e672538a4e6c223abc01
Mata07 <mmatisic7@gmail.com>
2021-04-22 09:52:09 +0200

    Section 10 - Add More Properties to Existing Users Table

---
+++ BulkyBook.Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    // Adding new tables to dbo.AspNetUsers
+    // We have to inherit from IdentityUser
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+        // what role user belongs to, not added to db
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
