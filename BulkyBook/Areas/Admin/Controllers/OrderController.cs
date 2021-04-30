d4436c5979e7404a7faa904e5c083b4a2cf75ffa
Mata07 <mmatisic7@gmail.com>
2021-04-30 15:21:06 +0200

    Section 19 - Order Javascript API Call

--- BulkyBook/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBook/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -30,13 +31,48 @@ namespace BulkyBook.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetOrderList()
+        public IActionResult GetOrderList(string status)
         {
-            var claimsIdentity = ClaimsIdentity
+            // get loggedin user
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
             IEnumerable<OrderHeader> orderHeaderList;
 
-            orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                // get all orders
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                // if it's not admin or employee get only orders for this user
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(
+                    u => u.ApplicationUserId == claim.Value, 
+                    includeProperties: "ApplicationUser");
+            }
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusApproved ||
+                                                            o.OrderStatus == SD.StatusInProcess ||
+                                                            o.OrderStatus == SD.StatusPending);
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusShipped);
+                    break;
+                case "rejected":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusCancelled ||
+                                                            o.OrderStatus == SD.StatusRefunded ||
+                                                            o.OrderStatus == SD.PaymentStatusRejected);
+                    break;
+                default:
+                    break;
+            }
 
             return Json(new { data = orderHeaderList });
         }
